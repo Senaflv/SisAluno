db78a761cd149782f78ee033dcff2921affc5b28
flavi <flavi@LAPTOP-7DARSQ94>
2016-11-29 16:18:26 -0200

    primeira versão do código

---
+++ src/java/br/com/sisaluno/web/Persist.java
@@ -0,0 +1,48 @@
+
+package br.com.sisaluno.web;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Persist {
+    
+    private String nome;
+    private String cpf;
+    private String telefone; 
+    private String nome_curso;
+      
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNome_curso() {
+        return nome_curso;
+    }
+
+    public void setNome_curso(String nome_curso) {
+        this.nome_curso = nome_curso;
+    }
+
+   
+}
